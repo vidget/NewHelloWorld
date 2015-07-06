4b7201ccee9865e12d89aaf2a90ed1f6b3d4c19b
Vidget <vidget@yahoo.com>
2015-07-06 14:30:14 -0400

    NewChange
    blahblah

--- NewHelloWorld/Program.cs
+++ NewHelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace NewHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Hello World");
             Console.Read();
 
         }
